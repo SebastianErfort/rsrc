d430aa0d51de3a5a3a22bcd9606e76384b2b0308
Sebastian Erfort <s.erfort@rug.nl>
2023-11-03 15:13:04 +0100

    update envy: mostly bash | cicd example deploy script

---
+++ bash/tempfile.sh
@@ -0,0 +1,2 @@
+temp_file=$(mktemp -p /tmp tmp.XXXXXXXXXX)
+echo "Created temp file:" "$temp_file"
--- bash/trap_signal.sh
+++ bash/trap_signal.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 # from https://www.howtogeek.com/814925/linux-signals-bash/
 
-trap graceful_shutdown SIGINT SIGQUIT SIGTERM
+trap "echo 'do not interrupt me!'" SIGINT # SIGQUIT SIGTERM
 
 graceful_shutdown()
 {
-  echo -e "\nRemoving temporary file:" $temp_file
+  echo -e "\nRemoving temporary file:" "$temp_file"
   rm -rf "$temp_file"
   exit
 }
 
 temp_file=$(mktemp -p /tmp tmp.XXXXXXXXXX)
-echo "Created temp file:" $temp_file
+echo "Created temporary file: ${temp_file}"
 
 counter=0
 
